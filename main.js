21cd8c2d449db9393e2bf9a594da193035d11710
Varvara Babkova <varvara.munday@gmail.com>
2019-08-08 08:57:26 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//const like = document.querySelector(".like")
+document.addEventListener("click", function(event){
+  if(event.target.className=="like"){
+    res = mimicServerCall()
+    .then(function(){
 
 
+      const like_glyph = event.target.firstElementChild
+      if (like_glyph.className =="like-glyph activated-heart"){
+        like_glyph.className = "like-glyph"
+         like_glyph.innerText = EMPTY_HEART
+      }else{
+         like_glyph.className = "like-glyph activated-heart"
+         like_glyph.innerText = FULL_HEART
+      }
+     
+      })
+      .catch(function(message) {
+
+        const modal = document.querySelector("#modal")
+        document.querySelector("#modal-message").innerText = message
+        modal.className = "not_hidden"
+
+        console.log(message)
+
+        setTimeout(function(){
+              modal.className = "hidden"
+
+        }, 2000);
+
+      });
+      console.log(res)
+    }
+})
 
 
 //------------------------------------------------------------------------------
